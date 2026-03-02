1989f114f3bf69200abddeaa6d1be478a0f7c166
mohanracharla16 <racharlamohan16@gmail.com>
2026-03-02 11:49:19 +0530

    Change default port to 5000 and remove use_reloader

--- app.py
+++ app.py
@@ -1480,6 +1480,6 @@ if __name__ == '__main__':
 
     
     # Run the Flask app
-    port = int(os.getenv("PORT", 8000))
-    app.run(debug=True, host='0.0.0.0', port=port, use_reloader=False)
+    port = int(os.getenv("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
     
